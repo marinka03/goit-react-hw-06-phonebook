f15c6bbbc8cec667b211f5978d87a7e50b99bad5
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-12-11 19:46:43 +0200

    add punctuation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
   return (
     <div className={style.container}>
         <h1 className={style.title}>Phonebook</h1>
-      <p className={style['add-contact-text']}>This is your phone book. Quickly add new contacts</p>
+      <p className={style['add-contact-text']}>This is your phone book. Quickly add new contacts...</p>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM804esPS1wqsxTHaK8wXq1p_wDtD15hJLUw&usqp=CAU" alt="Cat"  className={style.picture}/>
       <ContactForm/>
     </div>
